68c60af27f00dc972f1219065d7114946548c048
Robert Leon <llexi@hotmail.com>
2014-05-30 12:59:06 +0100

    added background image which is being drawn to the window

--- game.rb
+++ game.rb
@@ -5,8 +5,18 @@ require 'gosu'
 class GameWindow < Gosu::Window
   def initialize
     super(640, 480, false)
+    @background_image = Gosu::Image.new(self, "assets/background.png", true)
   end
-end
 
-window = GameWindow.new
-window.show
+	# 60 times per second by default
+	def update
+	end
+
+	# This happens immediately after each iteration of the update method
+	def draw
+		@background_image.draw(0, 0, 0)   
+	end
+end	
+
+	window = GameWindow.new
+	window.show
