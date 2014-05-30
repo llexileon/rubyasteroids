3eae43720231e4806060e5f53dfd149832b8d4ff
Robert Leon <llexi@hotmail.com>
2014-05-30 13:30:42 +0100

    created ship with rotation attribute

--- game.rb
+++ game.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby -w
 
 require 'gosu'
-require 'lib/player'
+require './lib/player'
 
 class GameWindow < Gosu::Window
   
   def initialize
     super(640, 480, false)
     @background_image = Gosu::Image.new(self, "assets/background.png", true)
-  
+    @player = Player.new(self)
+
   end
 
 	# 60 times per second by default
@@ -22,7 +23,9 @@ class GameWindow < Gosu::Window
 
 	# This happens immediately after each iteration of the update method
 	def draw
-		@background_image.draw(0, 0, 0)   
+		@background_image.draw(0, 0, 0)
+		@player.draw
+		
 	end
 
 end	
--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,9 @@
 class Player
   def initialize(window)
+  	@image = Gosu::Image.new(window, 'assets/ship.png', false)
+  end
+
+  def draw
+    @image.draw_rot(320, 240, 0, 0)
   end
 end
